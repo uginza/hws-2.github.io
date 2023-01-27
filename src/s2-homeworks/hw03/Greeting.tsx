8e4cb5eae5bb0a6f90bb96bea0abae8bd9b6e565
uginza <uginza@yandex.ru>
2023-01-27 10:55:36 +0300

    resultMayVary2

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -25,7 +25,7 @@ const Greeting: React.FC<GreetingPropsType> = (
         lastUserName,
     } // деструктуризация пропсов
 ) => {
-    const inputClass = s.errorInput // need to fix with (?:)
+    const inputClass = `${s.input} ${error ? s.errorInput : ''}`
     return (
         <div id={'hw3-form'} className={s.greetingForm}>
             <div className={s.text}>
--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -41,16 +41,8 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
     const [error, setError] = useState<string>('') // need to fix any
 
     const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
-        const trimName = e.currentTarget.value.trim()
-        if(trimName) {
-            setName(trimName)
-            error && setError('')
-        } else {
-            name && setName('')
-            setError("Ошибка! Введите имя!")
-        }
-        // setName(e.currentTarget.value) // need to fix
-        // error && setError('')
+        setName(e.currentTarget.value) // need to fix
+        error && setError('')
     }
     const addUser = () => {
         pureAddUser(name, setError, setName, addUserCallback)
